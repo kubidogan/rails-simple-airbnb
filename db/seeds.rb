74073a4139493cb50f4b30193316233cdfdc7d82
Kubi Dogan <kubi_g@hotmail.co.uk>
2022-02-16 16:27:05 +0000

    created seed and added faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+11.times do
+  Flat.create!(
+    name: Faker::Tea.variety,
+    address: Faker::Address.full_address,
+    description: Faker::Coffee.notes,
+    price_per_night: rand(30..100),
+    number_of_guests: rand(1..5)
+  )
+  puts 'created flats'
+end
